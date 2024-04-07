46730fbdb29386190bc2dccea8ef172c56f85f5f
Ilan Valencius <ilanvalencius@gmail.com>
2024-04-07 18:38:59 -0400

    working LubyMIS

--- src/main/scala/project_3/main.scala
+++ src/main/scala/project_3/main.scala
@@ -15,76 +15,169 @@ object main{
   Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
   Logger.getLogger("org.spark-project").setLevel(Level.WARN)
 
-  case class VertexInfo(vertexId: Long, result: Int, degreeOfInterest: Int)
+  // case class VertexInfo(vertexId: Long, result: Int, degreeOfInterest: Int)
+
+  // def LubyMIS(g_in: Graph[Int, Int]): Graph[Int, Int] = {
+  //   var remaining_vertices = g_in.numVertices
+  //   // updated_g is the graph we will shrink
+  //   var updated_g = g_in
+  //   // returned_g is a copy of g_in, and we modify whether each vertex has a 1 (in the MIS)
+  //   // OR a 0 (not in the MIS)
+  //   var returned_g = g_in
+  //   returned_g = returned_g.mapVertices {(id, attr) => -1 }
+  //   val rand = new scala.util.Random
+  //   var degrees = g_in.degrees
+  //   while (remaining_vertices >= 1) {
+  //     val newGraph = updated_g.mapVertices { case (vertexId, value) => {
+  //       val degreeOfInterest: Int = degrees.filter { case (vertexId2, _) => vertexId2 == vertexId }.first()._2
+  //       // get random integer 0 up to 2 * degreeOfInterest (exclusive)
+  //       // val random_int = rand.nextInt(2*degreeOfInterest)
+  //       // see if we get 0, so that the probability is set appropriately
+  //       // val result = if (random_int == 0) 1 else 0
+  //       val result = rand.nextInt(2)
+  //       VertexInfo(vertexId, result, degreeOfInterest)
+  //     }}
+  //     // here, we write the merge function such that we get the neighbor with the highest degree and with a 1
+  //     // or, if no neighbor has a 1, we get a neighbor with a 0. we do this so we can compare in the next step
+  //     val highestCompetingNeighbors: VertexRDD[VertexInfo] = newGraph.aggregateMessages[VertexInfo](
+  //       triplet => { // Map Function
+  //         triplet.sendToDst(triplet.srcAttr)
+  //         triplet.sendToSrc(triplet.dstAttr)
+  //       },
+  //       (vertexInfo1, vertexInfo2) => {
+  //         if(vertexInfo1.degreeOfInterest >= vertexInfo2.degreeOfInterest && vertexInfo1.result == 1) {
+  //           vertexInfo1
+  //         }
+  //         else if (vertexInfo2.result == 1) {
+  //           vertexInfo2
+  //         }
+  //         else {
+  //           vertexInfo1
+  //         }
+  //       }
+  //     )
+  //     // Join the original graph's vertices with the highestCompetingNeighbors RDD based on vertex ID
+  //     val joinedVertices = newGraph.vertices.join(highestCompetingNeighbors)
+
+  //     // Update the original graph's vertices based on the comparison
+  //     // "Yes" means we want to add vertex to MIS, "No" means we do not.
+  //     val decisions = joinedVertices.map { case (vertexId, (originalInfo, competingInfo)) =>
+  //       // Compare originalInfo and competingInfo and decide if vertex joins MIS
+  //       if (((originalInfo.degreeOfInterest >= competingInfo.degreeOfInterest) && originalInfo.result == 1)||(originalInfo.result == 1 && (competingInfo.result == -1))) {
+  //         (vertexId, "Yes")
+  //       } else {
+  //         (vertexId, "No")
+  //       }
+  //     }
+  //     // Filter decisions RDD to select vertices with a decision of "Yes"
+  //     val mappedVertices = returned_g.vertices.join(decisions).mapValues { case (value, decision) => if (decision == "Yes") 1 else -1 }
+  //     // we update returned_g with what we include in the MIS
+  //     returned_g = returned_g.mapVertices { case (vertexId, _) =>
+  //       mappedVertices.lookup(vertexId).headOption.getOrElse(0)
+  //     }
+  //     // now we go through our edges, delete everyone who has a "Yes" in the triplet. so we delete all vertices with a "Yes" as well as their neighbors
+  //     updated_g = updated_g.subgraph(epred = triplet => {
+  //       val srcDecision = decisions.lookup(triplet.srcId).headOption.getOrElse("No")
+  //       val dstDecision = decisions.lookup(triplet.dstId).headOption.getOrElse("No")
+  //       srcDecision != "Yes" && dstDecision != "Yes"
+  //     })
+  //     remaining_vertices = updated_g.numVertices
+  //     returned_g.cache()
+  //     updated_g.cache()
+  //   }
+  //   return returned_g
+  // }
+
+  case class VertexProperties(degree: Int, zero_or_one: Int, active: String)
+
+  // Define a function to see if any vertex is active
+  def anyActive(g: Graph[VertexProperties, Int]): Boolean = {
+    g.vertices.filter(
+      { case (_, prop) => prop.active == "active"}
+      ).count() > 0
+  }
 
   def LubyMIS(g_in: Graph[Int, Int]): Graph[Int, Int] = {
-    var remaining_vertices = g_in.numVertices
-    // updated_g is the graph we will shrink
-    var updated_g = g_in
-    // returned_g is a copy of g_in, and we modify whether each vertex has a 1 (in the MIS)
-    // OR a 0 (not in the MIS)
-    var returned_g = g_in
-    returned_g = returned_g.mapVertices {(id, attr) => -1 }
+    // Create random generator
     val rand = new scala.util.Random
-    var degrees = g_in.degrees
-    while (remaining_vertices >= 1) {
-      val newGraph = updated_g.mapVertices { case (vertexId, value) => {
-        val degreeOfInterest: Int = degrees.filter { case (vertexId2, _) => vertexId2 == vertexId }.first()._2
-        // get random integer 0 up to 2 * degreeOfInterest (exclusive)
-        val random_int = rand.nextInt(2*degreeOfInterest)
-        // see if we get 0, so that the probability is set appropriately
-        val result = if (random_int == 0) 1 else 0
-        VertexInfo(vertexId, result, degreeOfInterest)
-      }}
-      // here, we write the merge function such that we get the neighbor with the highest degree and with a 1
-      // or, if no neighbor has a 1, we get a neighbor with a 0. we do this so we can compare in the next step
-      val highestCompetingNeighbors: VertexRDD[VertexInfo] = newGraph.aggregateMessages[VertexInfo](
+    // Keep track of what vertex IDs are in the MIS
+    var mis_vertices = Set[Long]()
+    // Initialize graph to be modified (add properties)
+    val degrees = g_in.degrees
+    var degree_graph = g_in.outerJoinVertices(degrees)({ case (_, _, prop) => prop.getOrElse(0)})
+    var g_mod = degree_graph.mapVertices((_, degree) => VertexProperties(degree, -99, "active"))
+    println("Initial Size of graph: " + g_mod.numVertices)
+    // Each vertex now stored as (vertexId, (zero_or_one, active))
+    // Loop while active vertices exist
+    while(anyActive(g_mod)){
+      // Step 1 - set values
+      g_mod = g_mod.mapVertices((_, prop: VertexProperties) => {
+        // Don't touch inactive vertices
+        if (prop.active != "active") {
+          prop
+        } else {
+          // Set message to send to other vertices
+          val value = if (rand.nextDouble() < 1.0 / (2 * prop.degree)) 1 else 0
+          VertexProperties(prop.degree, value, prop.active)
+        }
+      })
+      // Step 2 - send message to neighbors and get highest competing neighbor
+      // Compare the degree of the neighbors and the zero_or_one value
+      val highest_neighbor: VertexRDD[VertexProperties] = g_mod.aggregateMessages[VertexProperties](
         triplet => { // Map Function
           triplet.sendToDst(triplet.srcAttr)
           triplet.sendToSrc(triplet.dstAttr)
         },
-        (vertexInfo1, vertexInfo2) => {
-          if(vertexInfo1.degreeOfInterest >= vertexInfo2.degreeOfInterest && vertexInfo1.result == 1) {
-            vertexInfo1
-          }
-          else if (vertexInfo2.result == 1) {
-            vertexInfo2
-          }
-          else {
-            vertexInfo1
+        // Should we check to see if neighbors are inactive??
+        (prop1, prop2) => {
+          if ((prop1.zero_or_one == 1) && (prop1.degree >= prop2.degree)) {
+            prop1 // First vertex beats its neighbor
+          } else if ((prop2.zero_or_one == 1) && (prop2.degree > prop1.degree)) {
+            prop2 // Other vertex has has a higher degree and is a 1 so it wins
+          } else {
+            prop1 // Other vertices can be higher but are all 0
           }
         }
       )
-      // Join the original graph's vertices with the highestCompetingNeighbors RDD based on vertex ID
-      val joinedVertices = newGraph.vertices.join(highestCompetingNeighbors)
-
-      // Update the original graph's vertices based on the comparison
-      // "Yes" means we want to add vertex to MIS, "No" means we do not.
-      val decisions = joinedVertices.map { case (vertexId, (originalInfo, competingInfo)) =>
-        // Compare originalInfo and competingInfo and decide if vertex joins MIS
-        if ((originalInfo.degreeOfInterest >= competingInfo.degreeOfInterest && originalInfo.result == 1)||(originalInfo.result == 1 && competingInfo.result == -1)) {
-          (vertexId, "Yes")
-        } else {
-          (vertexId, "No")
-        }
-      }
-      // Filter decisions RDD to select vertices with a decision of "Yes"
-      val mappedVertices = returned_g.vertices.join(decisions).mapValues { case (value, decision) => if (decision == "Yes") 1 else -1 }
-      // we update returned_g with what we include in the MIS
-      returned_g = returned_g.mapVertices { case (vertexId, _) =>
-        mappedVertices.lookup(vertexId).headOption.getOrElse(0)
-      }
-      // now we go through our edges, delete everyone who has a "Yes" in the triplet. so we delete all vertices with a "Yes" as well as their neighbors
-      updated_g = updated_g.subgraph(epred = triplet => {
-        val srcDecision = decisions.lookup(triplet.srcId).headOption.getOrElse("No")
-        val dstDecision = decisions.lookup(triplet.dstId).headOption.getOrElse("No")
-        srcDecision != "Yes" && dstDecision != "Yes"
+      // Join the original graph's vertices with the highest_neighbor RDD based on vertex ID
+      val joinedVertices = g_mod.vertices.join(highest_neighbor).filter({
+        case (_, (prop, competing)) => prop.active == "active"
       })
-      remaining_vertices = updated_g.numVertices
-      returned_g.cache()
-      updated_g.cache()
+      println("# of active vertices: " + joinedVertices.count())
+      // Filter out vertices that lose to one of their neighbors (i.e. neighbors are in MIS)
+      val message_comparison = joinedVertices.filter({
+        case (id, (prop: VertexProperties, competing: VertexProperties)) => {
+          // Vertex stays in the MIS if it beats out its highest_neighbor 
+          // Because we do this in parallel two vertices may tie (be neighbors of each other) so we don't do anything if this is the case
+          (prop.zero_or_one == 1) && (prop.degree > competing.degree) || (prop.zero_or_one == 1 && competing.zero_or_one == 0)
+        }
+      }).map({ case (id, (prop, competing)) => (id, prop)})
+      // Step 3 - Extract all vertices that have survived the comparison (will be added to MIS)
+      val vertexIds_mis = message_comparison.map({ case (vertexId, _) => vertexId}).collect().toSet
+      println("Number of vertices added to MIS: " + vertexIds_mis.size)
+      // Add to the MIS
+      mis_vertices = mis_vertices.union(vertexIds_mis)
+      // Step 4 - check if each vertex or a neighbor of each vertex is in the MIS
+      // Get the id of every vertex where the neighbor is in the MIS
+      val neighbor_mis = g_mod.triplets.filter(e => (vertexIds_mis.contains(e.srcId) || vertexIds_mis.contains(e.dstId)))
+      val vertexIds_mis_neighbors = neighbor_mis.map(e => e.srcId).collect().toSet.union(neighbor_mis.map(e => e.dstId).collect().toSet)
+      // Join the two sets (neighbors + vertices in MIS)
+      val vertexIds_mis_total = vertexIds_mis.toSet.union(vertexIds_mis_neighbors)
+      // Step 5 - update the graph and deactivate necessary vertices
+      g_mod = g_mod.mapVertices((id, prop) => {
+        if (vertexIds_mis_total.contains(id)) {
+          VertexProperties(prop.degree, -99, "inactive")
+        } else {
+          prop
+        }
+      })
+      // Cache the graph
+      g_mod.cache()
     }
-    return returned_g
+    // Set vertices to 1 if in MIS, 0 otherwise
+    val out_graph = g_in.mapVertices((id, _) => if (mis_vertices.contains(id)) 1 else -1)
+    // Return the graph
+    return out_graph
   }
 
   def hasNeighborWithAttributeOne(triplet: EdgeContext[Int, Int, Boolean], from: String): Boolean = {
